3c607f5e3b4264281f7b60db99ccc7f083214fa2
Agustin Navarro <agustinnavarro@MacBook-Pro-de-Agustin.local>
2020-08-18 17:23:08 +0200

    Start

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,13 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Button } from "react-bootstrap"
+import "./index.scss"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+export default function Index() {
+  return (
+    <div>
+      <h1 className="title">Hi people</h1>
+      <Button variant="primary">Click Me</Button>
+      <Button variant="danger">Danger</Button>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
-
-export default IndexPage
+  )
+}
